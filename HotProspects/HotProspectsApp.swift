907dc11d785571a5df110909c3b5269f93ed9593
Rob Ranf <rob@emiyaconsulting.com>
2024-04-10 09:47:30 -0700

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Rob Ranf on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
