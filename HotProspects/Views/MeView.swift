07670d6207550c998df5f47b07341da4756d6c6d
Rob Ranf <rob@emiyaconsulting.com>
2024-04-24 17:52:23 -0700

    adding function to save and share qr code

--- HotProspects/Views/MeView.swift
+++ HotProspects/Views/MeView.swift
@@ -11,6 +11,7 @@ import CoreImage.CIFilterBuiltins
 struct MeView: View {
     @AppStorage("name") private var name = "Anonymous"
     @AppStorage("emailAddress") private var emailAddress = "you@yoursite.com"
+    @State private var qrCode = UIImage()
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
     
@@ -24,13 +25,19 @@ struct MeView: View {
                     .textContentType(.emailAddress)
                     .font(.title)
                     .textInputAutocapitalization(.never)
-                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                Image(uiImage: qrCode)
                     .interpolation(.none)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .contextMenu {
+                        ShareLink(item: Image(uiImage: qrCode), preview: SharePreview("My QR Code", image: Image(uiImage: qrCode)))
+                    }
             }
             .navigationTitle("Your code")
+            .onAppear(perform: updateCode)
+            .onChange(of: name, updateCode)
+            .onChange(of: emailAddress, updateCode)
         }
     }
     
@@ -42,7 +49,12 @@ struct MeView: View {
                 return UIImage(cgImage: cgImage)
             }
         }
+        
         return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
+    
+    func updateCode() {
+        qrCode = generateQRCode(from: "\(name)\n\(emailAddress)")
     }
 }
 
